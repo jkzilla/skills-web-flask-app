2c5d06877f9b73cb36502605483be90cff485dd4
johannakate <jkgriffin@ucdavis.edu>
2015-04-28 16:15:29 -0700

    working flask app

--- application.py
+++ application.py
@@ -8,11 +8,11 @@ def homepageapp():
 
 @app.route('/submit', methods=['GET', 'POST'])
 def applying():
-	first = request.form("first-name"), 
-	last = request.form("last-name"), 
-	salary = request.form("salary"), 
-	job = request.form("job")
-	return render_template("/submit.html", first=first, last=last, job=job, salary=salary)
+	first = request.form["first-name"] 
+	last = request.form["last-name"] 
+	salary = request.form["salary"]
+	job = request.form["job"]
+	return render_template('/submit.html', first=first, last=last, job=job, salary=salary)
 if __name__ == "__main__":
 	app.debug = True
 	app.run()
